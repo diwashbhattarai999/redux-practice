6563980dc0a216563011104f8a4cb2561d91daaf
Diwash <diwashb999@gmail.com>
2023-03-21 17:50:17 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddPostForm from "./posts/AddPostForm";
+import PostList from "./posts/PostList";
+
+function App() {
+  return (
+    <div>
+      <PostList />
+      <AddPostForm />
+    </div>
+  );
+}
+
+export default App;
